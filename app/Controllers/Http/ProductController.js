7fb4423bc366327a857da57e32b5b3c9c282a286
Alexandre Magno <pedestaljunior@gmail.com>
2020-04-29 01:07:10 -0300

    new files

---
+++ app/Controllers/Http/ProductController.js
@@ -0,0 +1,86 @@
+'use strict'
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+
+
+const Product = use("App/Models/Product")
+/**
+ * Resourceful controller for interacting with products
+ */
+class ProductController {
+
+  /**
+   * Render a form to be used for creating a new product.
+   * GET products/create
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async create ({ request, response, view }) {
+    const product = await Product.create(request.all())
+    return product
+  }
+
+  /**
+   * Create/save a new product.
+   * POST products
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store ({ request, response }) {
+  }
+
+  /**
+   * Display a single product.
+   * GET products/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+  /**
+   * Render a form to update an existing product.
+   * GET products/:id/edit
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async edit ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update product details.
+   * PUT or PATCH products/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a product with id.
+   * DELETE products/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = ProductController
